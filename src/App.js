b088cbe472e035f857dbbb702c7c6fd2d0e017d9
raymondshum <rshum@csumb.edu>
2022-05-08 15:54:17 -0700

    Added AddStudent route to "/"

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Typography from '@mui/material/Typography';
 import SchedList from './components/SchedList';
 import Semester from './components/Semester';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import AddStudent from './components/AddStudent';
 
 function App() {
   return (
@@ -18,7 +19,10 @@ function App() {
       </AppBar>
       <BrowserRouter>
        <Switch>
-        <Route exact path='/' component={Semester} />
+        <Route exact path ='/'>
+          <AddStudent />
+          <Semester />
+        </Route>
         <Route path='/schedule' component={SchedList} />
        </Switch>
       </BrowserRouter>
